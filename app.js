d3e310419a0f30c214ebe584e1a147ce5cf139fe
Akalanka Weerasooriya <mailboxaaw@gmail.com>
2019-03-05 08:02:52 +0530

    make it better

--- app.js
+++ app.js
@@ -4,7 +4,16 @@ var db = require('./db');
 var AuthRouter = require('./routes/Authentication');
 var UserRouter = require('./routes/User');
 
+const path = require("path");
+
 app.use('/users', UserRouter);
 app.use('/api/auth', AuthRouter);
+
+app.use(express.static(path.join(__dirname, "client", "build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+
 
 module.exports = app;
