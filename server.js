3a4aa2a350e6f6f4420c80572b9d7e955db836fd
Akalanka Weerasooriya <mailboxaaw@gmail.com>
2019-03-05 21:52:24 +0530

    make it better

--- server.js
+++ server.js
@@ -1,12 +1,5 @@
 var app = require('./app');
-const path = require("path");
 var port = process.env.PORT || 5000;
-
-app.use(express.static(path.join(__dirname, "client", "build")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-});
 
 
 var server = app.listen(port, function() {
