a4d346804059433021e3f3265260814689c3b958
Mátyás Angyal <amatyas001@gmail.com>
2020-05-18 02:23:23 +0200

    fix(imports from loginHandler)

--- client/src/routes/EditItem.js
+++ client/src/routes/EditItem.js
@@ -1,20 +1,23 @@
-import React, { Component } from "react";
-import { Redirect } from "react-router-dom";
-import EditItemComp from "../components/item/EditItemComp";
-import LoginHandler from "../_helper/LogInHandler";
+import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import EditItemComp from '../components/item/EditItemComp';
+import { checkLoggedIn } from '../_helper/LogInHandler';
 
 class AddItem extends Component {
-  render() {
-    if (LoginHandler.checkLoggedIn()) {
-      return (
-        <div>
-          <EditItemComp history={this.props.history} itemId={this.props.match.params.id} />
-        </div>
-      );
-    } else {
-      return <Redirect to={{ pathname: "/" }} />;
-    }
-  }
+	render() {
+		if (checkLoggedIn()) {
+			return (
+				<div>
+					<EditItemComp
+						history={this.props.history}
+						itemId={this.props.match.params.id}
+					/>
+				</div>
+			);
+		} else {
+			return <Redirect to={{ pathname: '/' }} />;
+		}
+	}
 }
 
 export default AddItem;
