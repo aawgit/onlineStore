cd8366c4ae83118c0232706ffd08088db3ae3ca8
Mátyás Angyal <amatyas001@gmail.com>
2020-05-25 01:10:44 +0200

    config(build scripts added)

--- server/src/utils/Mailer.js
+++ server/src/utils/Mailer.js
@@ -1,14 +1,10 @@
 import nodemailer from 'nodemailer';
 
 /**
- * ### **NodeMailer**
- *
  * Available options
  *
  * - **from** - The email address of the sender. All email addresses can be plain ‘sender@server.com’ or formatted '“Sender Name” sender@server.com’, see Address object for details
  * - **to** - Comma separated list or an array of recipients email addresses that will appear on the To: field
- * - **cc** - Comma separated list or an array of recipients email addresses that will appear on the Cc: field
- * - **bcc** - Comma separated list or an array of recipients email addresses that will appear on the Bcc: field
  * - **subject** - The subject of the email
  * - **text** - The plaintext version of the message as an Unicode string, Buffer, Stream or an attachment-like object `{path: ‘/var/data/…'}`
  * - **html** - The HTML version of the message as an Unicode string, Buffer, Stream or an attachment-like object `{htmlStream: fs.createReadStream("content.html")}`
