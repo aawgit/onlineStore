468624e27ed869fb4b31e7b4942b8cb1295f92df
multi-vit <103585624+multi-vit@users.noreply.github.com>
2022-12-05 13:25:17 +0000

    Add unit test for getUsers by ID - correct bug on line 22 of user.controller.js

--- server/test/controller/User.test.js
+++ server/test/controller/User.test.js
@@ -1,21 +1,24 @@
 import express from "express";
 import userController from "../../src/controller/user.controller";
-import request from "supertest"
+import request from "supertest";
 import * as userService from "../../src/service/user.service";
-
 
 const app = express();
 
 app.use("/api/users", userController);
 
-
 describe("Test User Controller", () => {
   test("It should return a list of users", async () => {
-    jest.spyOn(
-      userService,'getUsers'
-    ).mockResolvedValue(['dummy user'])
+    jest.spyOn(userService, "getUsers").mockResolvedValue(["dummy user"]);
     const response = await request(app).get("/api/users");
     expect(response.statusCode).toBe(200);
-    expect(response.body).toEqual(['dummy user'])
+    expect(response.body).toEqual(["dummy user"]);
+  });
+  test("It should return a user with provided id", async () => {
+    jest.spyOn(userService, "getUsers").mockResolvedValue(["dummy user"]);
+    const response = await request(app).get("/api/users/1");
+    console.log(response);
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toEqual(["dummy user"]);
   });
 });
